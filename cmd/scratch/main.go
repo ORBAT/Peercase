8871e0ab41032e54b0fc3ef0e69550601da74e83
Tom Eklöf <tome@unity3d.com>
2018-04-04 10:55:23 +0300

    scratch for nodes

--- cmd/scratch/main.go
+++ cmd/scratch/main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
-	"fmt"
+	"io/ioutil"
+	"math/rand"
 	"strings"
-	"sync"
+	"time"
 
 	"github.com/ORBAT/Peerdoc/log"
+	"github.com/ORBAT/Peerdoc/pkg/crypto/hash"
+	"github.com/ORBAT/Peerdoc/pkg/crypto/sign"
 	"github.com/attic-labs/noms/go/chunks"
 	"github.com/attic-labs/noms/go/datas"
-	"github.com/attic-labs/noms/go/merge"
+	"github.com/attic-labs/noms/go/marshal"
+	"github.com/attic-labs/noms/go/nomdl"
 	"github.com/attic-labs/noms/go/types"
 	"go.uber.org/zap"
 )
@@ -23,154 +27,265 @@ func ToHuman(v types.Value) string {
 	return strings.Replace(types.EncodedValue(v), "\n", " ", -1)
 }
 
-func NewFile(ds datas.Dataset, content string) (file types.Struct, blobRef types.Ref) {
-	db := ds.Database()
-	blobRef = db.WriteValue(types.NewBlob(db, strings.NewReader(content)))
-	file = types.NewStruct("File", types.StructData{
-		"data": blobRef,
-	})
-
-	return
+type encryptedPayload struct {
+	Method      string
+	Fingerprint sign.Fingerprint
+	KeyIdx      uint32
+	Payload     types.Blob `noms:"original"`
 }
 
-func NewDir(ds datas.Dataset) (dir types.Struct, newDs datas.Dataset) {
-	db := ds.Database()
-	dir = types.NewStruct("Directory", types.StructData{
-		"entries": types.NewMap(db),
-	})
+type epRef struct {
+	Method      string
+	Fingerprint sign.Fingerprint
+	KeyIdx      uint32
+	PayloadRef  types.Ref `noms:"original"` // Ref<Blob>
+}
 
-	newDs, err := db.CommitValue(ds, dir)
+type CryptoInfo struct {
+	Method      string
+	Fingerprint sign.Fingerprint
+	KeyIdx      uint32
+}
+
+var cryptoInfoTempl = types.MakeStructTemplate("CryptoInfo", []string{"method", "fingerprint", "keyIdx"})
+
+func (ci CryptoInfo) MarshalNoms(vrw types.ValueReadWriter) (val types.Value, err error) {
+	fpNoms, err := ci.Fingerprint.MarshalNoms(vrw)
 	if err != nil {
-		panic("error committing new dir: " + err.Error())
+		return nil, err
+	}
+	ciNoms := cryptoInfoTempl.NewStruct([]types.Value{types.String(ci.Method), fpNoms, types.Number(ci.KeyIdx)})
+	return ciNoms, nil
+}
+
+func NewCryptoInfo(method string, fp sign.Fingerprint, keyIdx uint32) CryptoInfo {
+	return CryptoInfo{
+		Method:      method,
+		Fingerprint: fp,
+		KeyIdx:      keyIdx,
+	}
+}
+
+type EncryptedRef struct {
+	CryptoInfo
+	Payload types.Ref `noms:"original"` // Ref<Blob>
+}
+
+func (EncryptedRef) ContType() ContentType {
+	return ContentEncrRef
+}
+
+type EncryptedBlob struct {
+	CryptoInfo
+	Payload types.Blob `noms:"original"`
+}
+
+func (EncryptedBlob) ContType() ContentType {
+	return ContentEncrBlob
+}
+
+type Container struct {
+	Contents map[string]*Node
+}
+
+func (Container) ContType() ContentType {
+	return ContentContainer
+}
+
+type ContentType string
+
+const (
+	ContentContainer = ContentType("Container")
+	ContentEncrRef   = ContentType("EncryptedRef")
+	ContentEncrBlob  = ContentType("EncryptedBlob")
+)
+
+type Content interface {
+	ContType() ContentType
+}
+
+type Metadata struct {
+	Creator sign.Fingerprint
+	Ctime   types.Number
+	ID      string
+}
+
+type Node struct {
+	Metadata Metadata
+	Contents Content // see nomdl spec below
+	Parent   *Node   `noms:"original"` // Ref<Cycle<Node>>
+}
+
+type CreateEncType func() types.Struct
+
+func EncECIES(date []byte) types.Struct {
+	panic("WIP")
+}
+
+func NewMetadata(ctor sign.Fingerprint) Metadata {
+	nowUnix := time.Now().Unix()
+	md := Metadata{
+		Creator: ctor,
+		Ctime:   types.Number(nowUnix),
+	}
+	idbs := make([]byte, hash.ByteLen)
+	rand.Seed(nowUnix)
+	rand.Read(idbs)
+	h := hash.Of(idbs)
+	md.ID = h.String()
+	return md
+}
+
+func NewLeafNode(md Metadata, createBlob CreateEncType) *Node {
+	panic("WIP")
+}
+
+var nodeType = nomdl.MustParseType(`Struct Node {
+	metadata: Struct Metadata {
+		creator: Blob, // a fingerprint  
+		ctime: Number,
+		id: String,
+	},
+	contents: Struct Encrypted {
+		info: Struct CryptoInfo {
+			method: String,
+			fingerprint: Struct Fingerprint {
+				bytes: Blob,
+			},
+			keyIdx?: Number,
+		},
+		data: Ref<Blob> | Blob
+	} | Struct Container {
+		children: Map<String, Cycle<Node>>,
+	},
+	parent: Ref<Cycle<Node>>,
+}`)
+
+var cryptoInfoType, encryptedType, containerType *types.Type
+
+func init() {
+	nodeDesc := nodeType.Desc.(types.StructDesc)
+	contentType, _ := nodeDesc.Field("contents")
+	contentCompound := contentType.Desc.(types.CompoundDesc)
+
+	for _, elemType := range contentCompound.ElemTypes {
+		switch elemType.Desc.(types.StructDesc).Name {
+		case "Encrypted":
+			encryptedType = elemType
+		case "Container":
+			contentType = elemType
+		}
 	}
 
-	return
+	encryptedType, _ = nodeDesc.Field("contents")
+	cryptoInfoType, _ = encryptedType.Desc.(types.StructDesc).Field("info")
 }
 
-func AddFileTo(ds datas.Dataset, dir types.Struct, file types.Struct, name string) (newDir types.Struct, newDs datas.Dataset) {
-	db := ds.Database()
-	entries := dir.Get("entries").(types.Map)
-	entries = entries.Edit().Set(types.String(name), file).Map()
-	newDir = dir.Set("entries", entries)
+/*
+`Struct Inode {
+          attr: Struct Attr {
+            ctime: Number,
+            gid: Number,
+            mode: Number,
+            mtime: Number,
+            uid: Number,
+            xattr: Map<String, Blob>,
+          },
+          contents: Struct Symlink {
+              targetPath: String,
+            } | Struct File {
+              data: Ref<Blob>,
+            } | Struct Directory {
+              entries: Map<String, Cycle<Inode>>,
+            },
+        }`
+*/
 
-	newDs, err := db.CommitValue(ds, newDir)
-	if err != nil {
-		panic("error committing new file to dir: " + err.Error())
-	}
+/*
+`Struct Node {
+          attr: Struct Attr {
+            ctime: Number,
+            gid: Number,
+            mode: Number,
+            mtime: Number,
+            uid: Number,
+            xattr: Map<String, Blob>,
+          },
+          contents: Struct EncryptedPayload {
+              method: String,
 
-	return
-}
+              data: Ref<Blob>,
+            } | Struct Directory {
+              entries: Map<String, Cycle<Inode>>,
+            },
+        }`
+*/
 
 func main() {
 	logger := log.DevLogger("scratch")
 	defer logger.Sync()
+	_, db := NewMemNoms("ns")
 
-	log1 := logger.With("node", "node1")
-	log2 := logger.With("node", "node2")
+	r := rand.New(rand.NewSource(1))
 
-	//	fileType := nomdl.MustParseType(`
-	//		Struct File {
-	//			data: Ref<Blob>,
-	//		}`)
+	testEncPload(r, db, logger)
 
-	_, db1 := NewMemNoms("first")
-	ds1 := db1.GetDataset("files")
-	log1.Infow("created db1")
+	testEncPloadRef(r, db, logger)
 
-	dir1Struct, ds1 := NewDir(ds1)
-	log1.Infow("wrote root dir", "dir_hash", dir1Struct.Hash(), "struct", ToHuman(dir1Struct))
+}
 
-	file11Struct, file11BlobRef := NewFile(ds1, "file 1 contents")
-	log1.Infow("wrote file1", "blob_hash", file11BlobRef.TargetHash(), "struct", ToHuman(file11Struct))
-
-	file12Struct, file12BlobRef := NewFile(ds1, "file 2 contents")
-	log1.Infow("wrote file2", "blob_hash", file12BlobRef.TargetHash(), "struct", ToHuman(file12Struct))
-
-	dir1Struct, ds1 = AddFileTo(ds1, dir1Struct, file11Struct, "file1")
-	dir1Struct, ds1 = AddFileTo(ds1, dir1Struct, file12Struct, "file2")
-	log1.Infow("wrote two files to root", "dir_hash", dir1Struct.Hash(), "struct", ToHuman(dir1Struct))
-
-	log1.Infow("head status", "head1", ToHuman(ds1.Head()), "head_hash", ds1.Head().Hash(), "file11_struct_hash", file11Struct.Hash())
-
-	cs2, db2 := NewMemNoms("second")
-	ds2 := db2.GetDataset("files")
-	log2.Infow("created db2")
-
-	dir2Struct, ds2 := NewDir(ds2)
-	log2.Infow("wrote root dir", "dir_hash", dir2Struct.Hash(), "struct", ToHuman(dir2Struct))
-
-	file21Struct, file21BlobRef := NewFile(ds2, "file 1 contents")
-	log2.Infow("wrote file1", "blob_hash", file21BlobRef.TargetHash(), "struct", ToHuman(file21Struct))
-
-	file23Struct, file23BlobRef := NewFile(ds2, "file 3 contents")
-	log2.Infow("wrote file3", "blob_hash", file23BlobRef.TargetHash(), "struct", ToHuman(file23Struct))
-
-	dir2Struct, ds2 = AddFileTo(ds2, dir2Struct, file21Struct, "file1")
-	dir2Struct, ds2 = AddFileTo(ds2, dir2Struct, file23Struct, "file3")
-	log2.Infow("wrote two files to root", "dir_hash", dir2Struct.Hash(), "struct", ToHuman(dir2Struct))
-
-	log2.Infow("head status", "head", ToHuman(ds2.Head()), "head_hash", ds2.Head().Hash())
-
-	progCh := make(chan datas.PullProgress, 256)
-
-	var wg sync.WaitGroup
-	wg.Add(1)
-
-	go func(ch chan datas.PullProgress, wg *sync.WaitGroup) {
-		defer log2.Sync()
-		for prog := range ch {
-			log2.Infow("pull progressed", "prog_struct", fmt.Sprintf("%+v", prog))
-		}
-		log2.Info("progress channel closed")
-		wg.Done()
-	}(progCh, &wg)
-
-	// NOTE: ds2.HeadRef().TargetHash() == ds2.Head().Hash()
-
-	datas.Pull(db1, db2, ds1.HeadRef(), progCh)
-	close(progCh)
-	wg.Wait()
-
-	log2.Infow("did the pull go through?", "has_file12_blob_tgt", cs2.Has(file12BlobRef.TargetHash()), "has_file12_struct", cs2.Has(file12Struct.Hash()))
-
-	log2.Infow("head status", "head", ToHuman(ds2.Head()), "head_hash", ds2.Head().Hash())
-
-	log2.Infow("finding common ancestor for merge", "src", ds1.Head().Hash(), "our", ds2.Head().Hash())
-	anc, foundAnc := datas.FindCommonAncestor(ds1.HeadRef(), ds2.HeadRef(), db2)
-
-	log2.Infow("ancestor find done", "ok", foundAnc, "anc", anc.TargetHash())
-	if !foundAnc {
-		log2.DPanic("couldn't find ancestor")
+func testEncPloadRef(r *rand.Rand, db datas.Database, logger *zap.Logger) {
+	l := logger.Named("blob-ref")
+	ds := db.GetDataset("enc-ref-test")
+	fp := newFp(r)
+	blob := types.NewBlob(db, strings.NewReader("payload string ref"))
+	bref := db.WriteValue(blob)
+	pload := epRef{
+		Method:      "bla",
+		Fingerprint: fp,
+		PayloadRef:  bref,
+		KeyIdx:      666,
 	}
+	ploadVal, err := marshal.Marshal(db, pload)
+	l.Info("marshaled", zap.Error(err), zap.Stringer("bref.TargetHash()", bref.TargetHash()))
+	ds, err = db.CommitValue(ds, ploadVal)
+	l.Info("committed", zap.Error(err))
+	outVal := ds.HeadValue()
+	readPl := epRef{}
+	err = marshal.Unmarshal(outVal, &readPl)
+	l.Info("unmarshaled", zap.Error(err), zap.Any("readPl", readPl), zap.Stringer("readPl.PayloadRef.TargetHash", readPl.PayloadRef.TargetHash()))
 
-	// this won't happen with the setup above, it's here just for "completeness"
-	if anc.Equals(ds2.HeadRef()) {
-		log2.Infow("can do ff merge")
-		newDs2, err := db2.SetHead(ds2, ds1.HeadRef())
-		if err != nil {
-			log2.DPanic("SetHead went boom", zap.Error(err))
-		}
-		ds2 = newDs2
-	} else {
-		log2.Info("doing 3-way merge")
-		merged, err := merge.ThreeWay(ds2.HeadValue(),
-			ds1.HeadValue(),
-			anc.TargetValue(db2).(types.Struct).Get("value"),
-			db2, nil, nil)
-		if err != nil {
-			log2.DPanicw("merge fuckup", zap.Error(err))
-		}
-		newCommit := datas.NewCommit(merged, types.NewSet(db2, ds2.HeadRef(), ds1.HeadRef()), types.EmptyStruct)
-		commitRef := db2.WriteValue(newCommit)
-		log2.Infow("wrote new commit", "ref_tgt", commitRef.TargetHash())
-		if newds, err := db2.SetHead(ds2, commitRef); err != nil {
-			log2.DPanicw("SetHead fuckup", zap.Error(err))
-		} else {
-			ds2 = newds
-		}
+	actualPayload, err := ioutil.ReadAll(readPl.PayloadRef.TargetValue(db).(types.Blob).Reader())
+	l.Info("got payload from noms", zap.Error(err), zap.ByteString("actualPayload", actualPayload))
 
+}
+
+func testEncPload(r *rand.Rand, db datas.Database, logger *zap.Logger) {
+	l := logger.Named("just-blob")
+	ds := db.GetDataset("enc-test")
+	fp := newFp(r)
+	pl := encryptedPayload{
+		Method:      "bla",
+		Fingerprint: fp,
+		Payload:     types.NewBlob(db, strings.NewReader("payload string")),
+		KeyIdx:      666,
 	}
+	v, err := marshal.Marshal(db, pl)
+	l.Info("marshaled", zap.Error(err))
 
-	log2.Infow("head status", "head", ToHuman(ds2.Head()), "head_hash", ds2.Head().Hash())
+	ds, err = db.CommitValue(ds, v)
+	l.Info("committed", zap.Error(err))
 
+	outVal := ds.HeadValue()
+	outPl := encryptedPayload{}
+	err = marshal.Unmarshal(outVal, &outPl)
+	l.Info("unmarshaled", zap.Error(err), zap.Any("outPl", outPl))
+	outPayload, err := ioutil.ReadAll(outPl.Payload.Reader())
+	l.Info("got payload from noms", zap.Error(err), zap.ByteString("outPayload", outPayload))
+}
+
+func newFp(r *rand.Rand) sign.Fingerprint {
+	fp := make([]byte, sign.FingerprintLen)
+	r.Read(fp)
+	return sign.BytesToFingerprint(fp)
 }
